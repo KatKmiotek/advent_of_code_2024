3f1792f21f8a1cab511999e18cc574f55e49d443
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2024-12-05 19:56:41 +0000

    feat: remove broken

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -28,30 +28,12 @@ fn main() {
         .collect();
     println!("number of valid updates: {}", valid_updates.len());
 
-    let invalid_updates: Vec<_> = updates
-        .into_iter()
-        .filter(|update| !is_valid_update(update, &rules))
-        .collect();
-    println!("number of invalid updates: {}", invalid_updates.len());
-
-    let mut fixed_invalid_updates: Vec<Vec<i32>> = Vec::new();
-    for mut update in invalid_updates {
-        update.sort();
-        fixed_invalid_updates.push(update);
-    }
-
     let total_from_valid: i32 = valid_updates
         .iter()
         .map(|update| update[update.len() / 2])
         .sum();
 
-    let total_from_invalid: i32 = fixed_invalid_updates
-        .iter()
-        .map(|update| update[update.len() / 2])
-        .sum();
-
     println!("Total from valid is: {}", total_from_valid);
-    println!("Total from invalid is: {}", total_from_invalid);
 }
 
 fn is_valid_update(update: &[i32], rules: &[(i32, i32)]) -> bool {
